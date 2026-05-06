855fa0285d1f51179f33f0173fc984978db2e9f1
zaidebwini <bzxs0138@leeds.ac.uk>
2026-05-06 03:14:43 +0000

    qw

--- worksheet/task_2/task2.c
+++ worksheet/task_2/task2.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <string.h>
 
 int main(void) {
- 
-    srand(time(NULL)); // Seed the random generator
-    printf("Generate two numbers between 1-6 with total cannot be 7\n");
 
-    int num1, num2;
+    char hex[9]; // max 8 chars + null terminator
+    long decimal = 0;
 
-    do {
-        num1 = (rand() % 6) + 1; // 1–6
-        num2 = (rand() % 6) + 1; // 1–6
-    } while (num1 + num2 == 7);
+    scanf("%8s", hex);
 
-    printf("Number 1: %d\n", num1);
-    printf("Number 2: %d\n", num2);
-    printf("Sum: %d\n", num1 + num2);
+    for (int i = 0; i < strlen(hex); i++) {
+        decimal *= 16;
+
+        if (hex[i] >= '0' && hex[i] <= '9') {
+            decimal += hex[i] - '0';
+        }
+        else if (hex[i] >= 'A' && hex[i] <= 'F') {
+            decimal += hex[i] - 'A' + 10;
+        }
+        else if (hex[i] >= 'a' && hex[i] <= 'f') {
+            decimal += hex[i] - 'a' + 10;
+        }
+        else {
+            printf("Error: Invalid Hexadecimal\n");
+            return 0;
+        }
+    }
+
+    printf("%ld\n", decimal);
 
     return 0;
 }
